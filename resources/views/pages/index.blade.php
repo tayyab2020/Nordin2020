2cfffe57b0464fd8bb4c64198995d5bc6b151825
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-11-28 19:01:46 +0500

    updates

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -41,7 +41,7 @@
 
                                       $description = $blog->description;
                                       $description = preg_replace(array('#<[^>]+>#','#&nbsp;#'), ' ', $description);
-                                      $description = trim($description);
+                                      $description = preg_replace('/\s+/', ' ', $description);
                                       $words_limit = 230;
 
                                       $link = $blog->link;
