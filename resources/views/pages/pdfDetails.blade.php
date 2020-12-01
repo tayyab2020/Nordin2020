c0ed407079466e0d9b0d07ad51246707e0b43a29
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-12-01 14:40:51 +0500

    pdf updates

--- resources/views/pages/pdfDetails.blade.php
+++ resources/views/pages/pdfDetails.blade.php
@@ -2,14 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="GeniusOcean">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-    <!-- Bootstrap core CSS -->
+    {{--<!-- Bootstrap core CSS -->
     <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="{{asset('assets/css/font-awesome.min.css')}}" rel="stylesheet">
 
+
+    <link href="{{asset('assets/css/'.getcong('site_style').'.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet">--}}
 
 </head>
 
