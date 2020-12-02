3a875768e982f839dfb1f896fe7d52442b2b17f6
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-12-02 23:49:16 +0500

    fix

--- resources/views/admin/pages/addeditblog.blade.php
+++ resources/views/admin/pages/addeditblog.blade.php
@@ -181,7 +181,4 @@
 
 @endsection
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
