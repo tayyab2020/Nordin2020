f1a767686016c3ac25a012a44ce7223d802b483a
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-12-18 01:24:33 +0500

    updates

--- resources/views/_particles/slidersearch.blade.php
+++ resources/views/_particles/slidersearch.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container" style="margin-top: 8%;width: 100%;">
 
-        <h2 style="margin: 0;text-align: center;color: white;text-shadow: -3px 2px 11px #252424;font-size: 36px;position: relative;" class="css-ce6ko1 top-heading">Van ‘te koop’ naar ‘verkocht!’ Van ‘te huur’ naar ‘verhuurd.’ Welkom thuis!</h2>
+        <h2 style="margin: 0;text-align: center;color: white;text-shadow: -3px 2px 11px #252424;font-size: 36px;position: relative;" class="css-ce6ko1 top-heading">Samen lossen we het op.</h2>
 
     </div>
 
